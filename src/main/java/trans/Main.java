b86724a2b37a73bc865036261ec1a633d12b0e72
Dennis Neumann <neumann@sub.uni-goettingen.de>
2016-03-30 11:07:10 +0000

    Enlarge font

--- src/main/java/trans/Main.java
+++ src/main/java/trans/Main.java
@@ -1,29 +1,32 @@
 package trans;
 
+import java.awt.Font;
+
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.ScrollPaneConstants;
+import javax.swing.UIManager;
 
 public class Main {
 
 	public static void main(String[] args) {
+		UIManager.put("TextArea.font", new Font("Sans", Font.PLAIN, 22));
+
 		JTextArea text = new JTextArea();
 		text.setLineWrap(true);
 		text.setWrapStyleWord(true);
-		
-	    JScrollPane scroll = new JScrollPane ( text );
-	    scroll.setVerticalScrollBarPolicy ( ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS );
 
-		
-	    JFrame f = new JFrame("Translator");
-	    f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    f.setSize(500, 300);
-	    f.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		JScrollPane scroll = new JScrollPane(text);
+		scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+
+		JFrame f = new JFrame("Translator");
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setSize(500, 300);
+		f.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		f.add(scroll);
 		f.setVisible(true);
 
-		
 		MyListener listener = new MyListener();
 		text.addMouseListener(listener);
 
